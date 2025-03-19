1101263582d8d06d92c670694ca96e0c82c9f193
Kenny Null <timteillor@gmail.com>
2025-03-19 16:09:20 +0300

    Init Learn together app
    * The text has been moved to drawable
    * The image as been moved to drawable
    * Added image to TopBar
    * Add padding to title

---
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearnTogetherTheme {
+                MyScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyScreen() {
+    Scaffold(
+        topBar = { TopAppBar() },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text(
+                stringResource(R.string.title_text),
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+            Text(
+                stringResource(R.string.short_description_text)
+            )
+            Text(
+                stringResource(R.string.full_description_text)
+            )
+        }
+    }
+}
+
+@Composable
+fun TopAppBar() {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LearnTogetherTheme {
+        MyScreen()
+    }
+}
