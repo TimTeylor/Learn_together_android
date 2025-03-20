1acc3e725f2ba2a891ce54c6ab9e76abc2eaffe6
Kenny Null <timteillor@gmail.com>
2025-03-20 09:28:00 +0300

    Add paddings and align
    * Add padding to short*_text
    * Add padding to full*_text
    * Add align justify for short*_text
    * Add align justify for full*_text

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,10 +45,14 @@ fun MyScreen() {
                 modifier = Modifier.padding(16.dp)
             )
             Text(
-                stringResource(R.string.short_description_text)
+                stringResource(R.string.short_description_text),
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(horizontal = 16.dp)
             )
             Text(
-                stringResource(R.string.full_description_text)
+                stringResource(R.string.full_description_text),
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(16.dp)
             )
         }
     }
